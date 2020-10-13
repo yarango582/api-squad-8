090969583bca115a10ba36cece8bdbe4988892cf
Mauricio Guerra <maurogebe.96@gmail.com>
2020-10-13 00:14:37 -0500

    Adaptando la rama form con la master y haciendole unos ajustes para compatibilidad, agrego de condicionales

--- js/main.js
+++ js/main.js
@@ -1,19 +1,57 @@
 
 function getQuestionsNumbers(){
-
     let questionsQuantity = document.getElementById('questions-number').value;
-
     (questionsQuantity === '') ? questionsQuantity = 5: 'nada por aca';
-
     return questionsQuantity;
 }
 
-// function getQuestions() {
-//     let numberQuestions = getQuestionsNumbers();
-//     fetch(`https://opentdb.com/api.php?amount=${numberQuestions}`)
-//         .then(response => response.json())
-//         .then(data => printCard(data.results))
-// }
+function getQuestions() {
+    let numberQuestions = getQuestionsNumbers();
+    const questionsCategory = document.getElementById('questions-category').value
+    const questionsDificulty = document.getElementById('questions-dificulty').value
+    const questionsType = document.getElementById('questions-type').value
+    switch(true) {
+        case (questionsCategory === 'false' && questionsDificulty === 'false' && questionsType === 'false'):
+            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}`)
+                .then(response => response.json())
+                .then(data => printCardsQuestions(data.results))
+            break
+        case (questionsCategory === 'false' && questionsDificulty === 'false'):
+            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&type=${questionsType}`)
+                .then(response => response.json())
+                .then(data => printCardsQuestions(data.results))
+            break
+        case (questionsCategory === 'false' && questionsType === 'false'):
+            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&difficulty=${questionsDificulty}`)
+                .then(response => response.json())
+                .then(data => printCardsQuestions(data.results))
+            break
+        case (questionsDificulty === 'false' && questionsType === 'false'):
+            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&category=${questionsCategory}`)
+                .then(response => response.json())
+                .then(data => printCardsQuestions(data.results))
+            break
+        case (questionsCategory === 'false'):
+            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&difficulty=${questionsDificulty}&type=${questionsType}`)
+                .then(response => response.json())
+                .then(data => printCardsQuestions(data.results))
+            break
+        case (questionsDificulty === 'false'):
+            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&category=${questionsCategory}&type=${questionsType}`)
+                .then(response => response.json())
+                .then(data => printCardsQuestions(data.results))
+            break
+        case (questionsType === 'false'):
+            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&category=${questionsCategory}&difficulty=${questionsDificulty}`)
+                .then(response => response.json())
+                .then(data => printCardsQuestions(data.results))
+            break
+        default:
+            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&category=${questionsCategory}&difficulty=${questionsDificulty}&type=${questionsType}`)
+                .then(response => response.json())
+                .then(data => printCardsQuestions(data.results))
+    }
+}
 
 function getCategory() {
 
@@ -27,7 +65,7 @@ function listCategory(category){
 
     let formCategory = document.getElementById('category');
     formCategory.innerHTML = '';
-
+    formCategory.innerHTML = `<option value="all">Todas las Categorias</option>`
     category.forEach((question) => {
         let row = `<option value="${question.id}">${question.name}</option>`;
         formCategory.innerHTML += row;
@@ -35,4 +73,4 @@ function listCategory(category){
 
 }
 
-getCategory();
+getCategory()
