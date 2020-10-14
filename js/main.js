2531fbb977cd60cb937ee39906aaec8ea0b02f70
loky27 <hef6666qgmail.com>
2020-10-14 10:44:02 -0500

    libre de bugs

--- js/main.js
+++ js/main.js
@@ -1,91 +1,105 @@
-x
-function getQuestionsNumbers(){
-    let questionsQuantity = document.getElementById('questions-number').value;
-    (questionsQuantity === '') ? questionsQuantity = 5: 'nada por aca';
-    return questionsQuantity;
+function getQuestionsNumbers() {
+	let questionsQuantity = document.getElementById("questions-number").value;
+	questionsQuantity === "" ? (questionsQuantity = 5) : "nada por aca";
+	return questionsQuantity;
 }
 
 function getQuestions() {
-    let numberQuestions = getQuestionsNumbers();
-    const questionsCategory = document.getElementById('category').value
-    const questionsDificulty = document.getElementById('difficulty').value
-    const questionsType = document.getElementById('type').value
-    switch(true) {
-        case (questionsCategory === 'all' && questionsDificulty === 'all' && questionsType === 'all'):
-            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}`)
-                .then(response => response.json())
-                .then(data => printCardsQuestions(data.results))
-            break
-        case (questionsCategory === 'all' && questionsDificulty === 'all'):
-            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&type=${questionsType}`)
-                .then(response => response.json())
-                .then(data => printCardsQuestions(data.results))
-            break
-        case (questionsCategory === 'all' && questionsType === 'all'):
-            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&difficulty=${questionsDificulty}`)
-                .then(response => response.json())
-                .then(data => printCardsQuestions(data.results))
-            break
-        case (questionsDificulty === 'all' && questionsType === 'all'):
-            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&category=${questionsCategory}`)
-                .then(response => response.json())
-                .then(data => printCardsQuestions(data.results))
-            break
-        case (questionsCategory === 'all'):
-            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&difficulty=${questionsDificulty}&type=${questionsType}`)
-                .then(response => response.json())
-                .then(data => printCardsQuestions(data.results))
-            break
-        case (questionsDificulty === 'all'):
-            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&category=${questionsCategory}&type=${questionsType}`)
-                .then(response => response.json())
-                .then(data => printCardsQuestions(data.results))
-            break
-        case (questionsType === 'all'):
-            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&category=${questionsCategory}&difficulty=${questionsDificulty}`)
-                .then(response => response.json())
-                .then(data => printCardsQuestions(data.results))
-            break
-        default:
-            conditionalQuestion = fetch(`https://opentdb.com/api.php?amount=${numberQuestions}&category=${questionsCategory}&difficulty=${questionsDificulty}&type=${questionsType}`)
-                .then(response => response.json())
-                .then(data => printCardsQuestions(data.results))
-    }
+	let numberQuestions = getQuestionsNumbers();
+	const questionsCategory = document.getElementById("category").value;
+	const questionsDificulty = document.getElementById("difficulty").value;
+	const questionsType = document.getElementById("type").value;
+	switch (true) {
+		case questionsCategory === "all" &&
+			questionsDificulty === "all" &&
+			questionsType === "all":
+			conditionalQuestion = fetch(
+				`https://opentdb.com/api.php?amount=${numberQuestions}`
+			)
+				.then((response) => response.json())
+				.then((data) => printCardsQuestions(data.results));
+			break;
+		case questionsCategory === "all" && questionsDificulty === "all":
+			conditionalQuestion = fetch(
+				`https://opentdb.com/api.php?amount=${numberQuestions}&type=${questionsType}`
+			)
+				.then((response) => response.json())
+				.then((data) => printCardsQuestions(data.results));
+			break;
+		case questionsCategory === "all" && questionsType === "all":
+			conditionalQuestion = fetch(
+				`https://opentdb.com/api.php?amount=${numberQuestions}&difficulty=${questionsDificulty}`
+			)
+				.then((response) => response.json())
+				.then((data) => printCardsQuestions(data.results));
+			break;
+		case questionsDificulty === "all" && questionsType === "all":
+			conditionalQuestion = fetch(
+				`https://opentdb.com/api.php?amount=${numberQuestions}&category=${questionsCategory}`
+			)
+				.then((response) => response.json())
+				.then((data) => printCardsQuestions(data.results));
+			break;
+		case questionsCategory === "all":
+			conditionalQuestion = fetch(
+				`https://opentdb.com/api.php?amount=${numberQuestions}&difficulty=${questionsDificulty}&type=${questionsType}`
+			)
+				.then((response) => response.json())
+				.then((data) => printCardsQuestions(data.results));
+			break;
+		case questionsDificulty === "all":
+			conditionalQuestion = fetch(
+				`https://opentdb.com/api.php?amount=${numberQuestions}&category=${questionsCategory}&type=${questionsType}`
+			)
+				.then((response) => response.json())
+				.then((data) => printCardsQuestions(data.results));
+			break;
+		case questionsType === "all":
+			conditionalQuestion = fetch(
+				`https://opentdb.com/api.php?amount=${numberQuestions}&category=${questionsCategory}&difficulty=${questionsDificulty}`
+			)
+				.then((response) => response.json())
+				.then((data) => printCardsQuestions(data.results));
+			break;
+		default:
+			conditionalQuestion = fetch(
+				`https://opentdb.com/api.php?amount=${numberQuestions}&category=${questionsCategory}&difficulty=${questionsDificulty}&type=${questionsType}`
+			)
+				.then((response) => response.json())
+				.then((data) => printCardsQuestions(data.results));
+	}
 }
 
 function getCategory() {
-
-    fetch(`https://opentdb.com/api_category.php`)
-        .then(response => response.json())
-        .then(data => listCategory(data.trivia_categories))
-    
+	fetch(`https://opentdb.com/api_category.php`)
+		.then((response) => response.json())
+		.then((data) => listCategory(data.trivia_categories));
 }
 
-function listCategory(category){
-
-    let formCategory = document.getElementById('category');
-    formCategory.innerHTML = '';
-    formCategory.innerHTML = `<option value="all">Todas las Categorias</option>`
-    category.forEach((question) => {
-        let row = `<option value="${question.id}">${question.name}</option>`;
-        formCategory.innerHTML += row;
-    });
-
-}
-
-
-
-function printCardsQuestions(questions) {
-    
-	const container = document.getElementById("container");
-	container.innerHTML = "";
-	questions.forEach((question,index) => {
-        question.id=index;
-		const card = returnCard(question);
-		container.innerHTML += card;
+function listCategory(category) {
+	let formCategory = document.getElementById("category");
+	formCategory.innerHTML = "";
+	formCategory.innerHTML = `<option value="all">Todas las Categorias</option>`;
+	category.forEach((question) => {
+		let row = `<option value="${question.id}">${question.name}</option>`;
+		formCategory.innerHTML += row;
 	});
 }
 
+getCategory();
+
+function printCardsQuestions(questions) {
+	const container = document.getElementById("container");
+	const button = document.getElementById("button");
+	container.innerHTML = "";
+	questions.forEach((question, index) => {
+		question.id = index;
+		const card = returnCard(question);
+		container.innerHTML += card;
+    });
+    button.innerHTML+=``
+	button.innerHTML += `<button class="btn btn-primary button" onclick="imprimeresultado('${questions.length-1}')">enviar</button>`;
+}
 function returnCard(x) {
 	const card = `<div class="card mb-5">
                     <div class="card-body">
@@ -98,22 +112,21 @@ function returnCard(x) {
 												)}           
                     </div>
                 </div>`;
-                return card;
+	return card;
 }
 
-function returnAnswersHTML(correct, incorrects,ids) {
-    incorrects.push(correct)
+function returnAnswersHTML(correct, incorrects, ids) {
+	incorrects.push(correct);
 	let incorrectHTML = "";
-	incorrects.forEach((incorrect,index) => {
-        incorrectHTML += `<div class="form-check">
-        <input class="form-check-input" type="radio" name="${ids}" id="${ids}--${index}" value="${incorrect}" checked>
-        <label class="form-check-label" for="of${index}">
-        ${incorrect}
-        </label>
-        </div>`;
-	});
-    
+	for (i = 0; i < incorrects.length; i++) {
+		let al = Math.floor(Math.random() * incorrects.length);
+	incorrectHTML += `<div class="form-check">
+    <input class="form-check-input" type="radio" name="${ids}" id="${ids}-${i}" value="${incorrects[i]}" checked>
+    <label class="form-check-label" for="${ids}-${i}">
+    ${incorrects[i]}
+    </label>
+    </div>`;
+		
+	}
 	return incorrectHTML;
 }
-getCategory()
-
